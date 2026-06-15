adc930378414f0c992eba3ea97ebab64fe9f07cc
Kukuh TW <kukuhtw@gmail.com>
2026-06-15 18:43:20 +0700

    feat: add plugin module root

---
+++ src/plugins/mod.rs
@@ -0,0 +1,8 @@
+// src/plugins/mod.rs
+//
+// Application plugin extension points.
+//
+// The first supported plugin category is payment. More categories can be added
+// later, for example storage, transcoding, notifications, and job queues.
+
+pub mod payment;
